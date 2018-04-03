640c88584b14142bfa31ca7dbeb950ab4aad7cd2
Jonathan Stacks <jonstacks13@gmail.com>
2018-04-03 13:50:03 -0500

    Download the whole rds log file

--- pgbadger/download-rds-logs.sh
+++ pgbadger/download-rds-logs.sh
@@ -5,5 +5,5 @@ RDS_IDENTIFIER="$1"
 for f in $(aws rds describe-db-log-files --db-instance-identifier $RDS_IDENTIFIER | awk '{print $3}')
 do
   echo "Downloading $f.."
-  aws rds download-db-log-file-portion --db-instance-identifier $RDS_IDENTIFIER --log-file-name $f --output text > $(basename $f)
+  aws rds download-db-log-file-portion --db-instance-identifier $RDS_IDENTIFIER --log-file-name $f --starting-token 0 --output text > $(basename $f)
 done
